2f3e25b5c7ae8b0627272655deabc7667ac5530b
ZHENSHUO ZHAO <s3992237@student.rmit.edu.au>
2025-09-30 16:45:41 +1000

    My solution

--- 2-understanding-typescript-features/challenge/users.ts
+++ 2-understanding-typescript-features/challenge/users.ts
@@ -1,21 +1,24 @@
+type User = { id: number; name: string; role: string };
+type Age = { age: number };
+
 // Sample data
-const data = [
-  { id: 1, name: 'Zia', role: 'admin' },
-  { id: 2, name: 'Gaia', role: 'user' },
-  { id: 3, name: 'Noora', role: 'guest' }
+const data: User[] = [
+  { id: 1, name: "Zia", role: "admin" },
+  { id: 2, name: "Gaia", role: "user" },
+  { id: 3, name: "Noora", role: "guest" },
 ];
 
 // Function to find a user by ID
-function findUserById(id) {
-  return data.find(user => user.id === id);
+function findUserById(id: number): User | undefined {
+  return data.find((user) => user.id === id);
 }
 
 // Function to log user details
-function logUserDetails(user) {
+function logUserDetails(user: User): void {
   console.log(`Name: ${user.name}, Role: ${user.role}`);
 }
 
 // Function to merge two objects: a user and an age key value pair { age: 22 }
-function mergeObjects(user, age) {
+function mergeObjects(user: User, age: Age): User & Age {
   return { ...user, ...age };
 }
